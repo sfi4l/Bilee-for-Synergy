303d60e26463f59cdf341fb23700f326c059f380
Mrcavas <sidorov.mrcavas@mail.ru>
2023-09-23 12:17:40 +0700

    add tg lib and return border

--- src/CardSotrudnika/index.jsx
+++ src/CardSotrudnika/index.jsx
@@ -5,31 +5,17 @@ import menu from "../Img/Menu.svg"
 import back from "../Img/Back.png"
 
 const CardEmployee = () => {
-    return(
-        <div className={style.CardEmpCont}>
-             <div className={style.CardEmpContUp}>
-
-
-
-
-             <div className={style.CardEmpTxt}>
-                <img src={back} className={style.CardEmpMenuSvgBck}/>
-                <p className={style.CardEmpLogoText}>Bilee</p>
-                <img src={menu} className={style.CardEmpMenuSvg} />
-            </div> 
-
-
-
-
-
-             </div>
-
-
-
+  return (
+    <div className={style.CardEmpCont}>
+      <div className={style.CardEmpContUp}>
+        <div className={style.CardEmpTxt}>
+          <img src={back} className={style.CardEmpMenuSvgBck} />
+          <p className={style.CardEmpLogoText}>Bilee</p>
+          <img src={menu} className={style.CardEmpMenuSvg} />
         </div>
-            
-   
-    )
+      </div>
+    </div>
+  )
 }
 
 export default CardEmployee
