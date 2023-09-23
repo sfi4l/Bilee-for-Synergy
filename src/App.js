de4185500ed1606d39122aac036559055e799cb1
Mrcavas <sidorov.mrcavas@mail.ru>
2023-09-23 13:07:21 +0700

    make it actually appear

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { useInitData } from "@vkruglikov/react-telegram-web-app"
 import MainMenu from "../src/MainMenu/index"
+import Debug from "../src/Debugging/debug"
 import './App.css';
 import CardEmployee from "./CardSotrudnika";
 import Header from "./Header/index";
 
 function App() {
-  return (
-    <MainMenu></MainMenu>
-  )
+  const [initData, _] = useInitData()
+
+  if (initData) {
+    return <Debug/>
+  }
+
+  return <MainMenu/>
 }
 
 export default App;
