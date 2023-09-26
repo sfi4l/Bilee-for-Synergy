7b9bc11dbea714eb202b3965d128c55940cc6fdc
Mrcavas <sidorov.mrcavas@mail.ru>
2023-09-26 21:26:43 +0700

    change page transitions a bit

--- src/Routing.js
+++ src/Routing.js
@@ -26,9 +26,10 @@ const RootContainer = () => {
     <AnimatePresence mode="popLayout">
       <motion.div
         key={location.pathname}
-        initial={{ opacity: 0, x: 50 }}
+        initial={{ opacity: 0, x: 100 }}
         animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: 50 }}
+        exit={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.3 }}
       >
         <AnimatedOutlet />
       </motion.div>
