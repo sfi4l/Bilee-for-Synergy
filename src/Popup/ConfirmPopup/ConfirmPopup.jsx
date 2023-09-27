847e92b8d36e6b90192f0ec0d9ff2002ab55f8de
Mrcavas <sidorov.mrcavas@mail.ru>
2023-09-27 15:32:36 +0700

    confirmpopup fix :3

--- src/Popup/ConfirmPopup/ConfirmPopup.jsx
+++ src/Popup/ConfirmPopup/ConfirmPopup.jsx
@@ -6,9 +6,8 @@ import Popup from "../Popup/Popup"
 const ConfirmPopup = ({ onConfirm, onCancel }) => {
   return (
     <Popup>
-      <Text size="24px" align="center" margin="40px 0px 27px 0px">
-        {/* Это точно та кнопка? Подтвердите действие “Удалить” */}
-        Изменить помощь:
+      <Text size="24px" align="center" margin="40px 0px 47px 0px">
+        Это точно та кнопка? Подтвердите действие “Удалить”
       </Text>
 
       <Button highlightColor="accent_color" onClick={onConfirm}>
