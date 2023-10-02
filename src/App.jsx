5d49e2086b77ddaa1819b07e0c1e4828314ae4fb
sfi4l <21sfi4l@gmail.com>
2023-10-02 17:26:26 +0300

    Starting localization (Menu:main, Employee)
    Quack

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,83 @@ import { useEffect, useState } from "react"
 
 export let setPopup
 
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: {
+        menu: {
+          main: {
+            title: "Hi! This is Your menu",
+            discounts: "Discounts",
+            Ass: "Ass",
+            wehavelaunched: "We have launched",
+            shockcontent: "Shock Content",
+            uhaveentries: "You have N entries for today",
+            fullness: "Fullness",
+            employeecard: "Employee card",
+            company: "Company",
+            settings: "Settings",
+            service: "Services",
+            employees: "Employee",
+            subscription: "Subscription",
+          },
+          employee: {
+            title: "Nikolay,your card",
+            recordingisavailable: "Recording is available",
+            nolimit: "No limit",
+            adress: "Pushkina, 1",
+            summstatistics: "Summary statistics",
+            totalentries: "Total entries",
+            uniqueclients: "Unique clients",
+            averagecheck: "Average check",
+            totalrevenue: "Total revenue",
+            urservices: "Your services",
+          }
+        }
+      }
+    }
+  },
+  ru: {
+    translation: {
+      menu: {
+        main: {
+          title: "Привет! Это Ваше меню",
+          discounts: "Скидки",
+          Ass: "Жопа",
+          wehavelaunched: "Мы запустились!",
+          shockcontent: "Шок Контент",
+          uhaveentries: "На сегодня у Вас N записей",
+          fullness: "Заполненность",
+          employeecard: "Карточка сотрудника",
+          company: "Предприятие",
+          settings: "Настройки",
+          service: "Услуги",
+          employees: "Сотрудники",
+          subscription: "Подписка",
+        },
+        employee: {
+          title: "Николай, Ваша карточка",
+          recordingisavailable: "Запись доступна",
+          nolimit: "Без лимита",
+          adress: "Пушкина, 1",
+          summstatistics: "Сводная статистика",
+          totalentries: "Всего записей",
+          uniqueclients: "Уникальных клиентов",
+          averagecheck: "Средний чек",
+          totalrevenue: "Общая выручка",
+          urservices: "Ваши услуги",
+        }
+      }
+    }
+  },
+  lng: "ru",
+  fallbackLng: "ru",
+});
+
+
 const App = () => {
   const theme = useTheme()
   // const theme = Themes.dark
@@ -40,7 +117,7 @@ const App = () => {
       >
         <AnimatePresence>{popup}</AnimatePresence>
         <AnimatePresence mode="wait">
-          <RouterProvider router={router}/>
+          <RouterProvider router={router} />
         </AnimatePresence>
       </div>
     </ThemeContext.Provider>
